8fc1d4d868b9883fbaf9a1b05e969312648453b0
Vitor Capelli <vitinhocapelli18@gmail.com>
2025-05-01 17:20:38 -0300

    Corrige o index para o logRoutes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logRoutes = require('./routes/logRoutes');
+
 require('dotenv').config();
 
 console.log("MONGO_URI:", process.env.MONGO_URI);
