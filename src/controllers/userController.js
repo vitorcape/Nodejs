5ab4c378398d04c77181a9e746653e4ce785e3e8
Vitor Capelli <vitinhocapelli18@gmail.com>
2025-05-01 15:34:30 -0300

    Remove a duplicata da declaração de User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
 
 // Criar novo usuário
 exports.createUser = async (req, res) => {
