c312c85f5564a5cada0cccd7128cf57b79a4ac0f
Vitor Capelli <vitinhocapelli18@gmail.com>
2025-05-01 18:50:56 -0300

    Corrige a imagem do usuário Discord

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     senha: { type: String, required: true },
     idade: Number,
+    avatarUrl: String,
     role: { type: String, enum: ['user', 'admin'], default: 'user' }
 }, {
     timestamps: true
