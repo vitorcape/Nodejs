c22753df80ceb7c1dd1fd5c6840b38359ca3db49
Vitor Capelli <vitinhocapelli18@gmail.com>
2025-05-01 17:13:20 -0300

    Corrige CORS para acesso local

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 const userRoutes = require('./routes/userRoutes'); // <-- esse require aqui!
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',  // ou use '*' para liberar tudo (temporariamente)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/logs', logRoutes);
 
