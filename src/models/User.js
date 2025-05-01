2af07ce928f0203f8aa69b04d67aacf30fe591a2
Vitor Capelli <vitinhocapelli18@gmail.com>
2025-05-01 16:17:18 -0300

    Corrige o campo de nickname no cadastro

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     nome: { type: String, required: true },
-    user: { type: String, required: true },
+    nickname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     senha: { type: String, required: true },
     idade: Number,
